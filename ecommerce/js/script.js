b2cb62d12af0956f20151ea965549b90f6d0ecc2
rajvlogf <111104411+rajvlogf@users.noreply.github.com>
2022-09-18 22:26:04 +0530

    ecommerce project added

---
+++ ecommerce/js/script.js
@@ -0,0 +1,28 @@
+let mainphoto = document.querySelectorAll(".mainphoto");
+
+function changePhoto(src){
+    mainphoto[0].src = src;   
+}
+function changePhoto1(src){
+    mainphoto[1].src = src;   
+}
+function changePhoto2(src){
+    mainphoto[2].src = src;   
+}
+function changePhoto3(src){
+    mainphoto[3].src = src;   
+}
+function changePhoto4(src){
+    mainphoto[4].src = src;   
+}
+function changePhoto5(src){
+    mainphoto[5].src = src;   
+}
+function changePhoto6(src){
+    mainphoto[6].src = src;   
+}
+function changePhoto7(src){
+    mainphoto[7].src = src;   
+}
+
+
