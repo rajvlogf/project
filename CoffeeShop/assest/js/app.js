6ef694fcc18df884bc1d435b9cbb729db3afef04
rajvlogf <111104411+rajvlogf@users.noreply.github.com>
2022-10-12 00:22:04 +0530

    Add files via upload
    Coffee Cafe Project

---
+++ CoffeeShop/assest/js/app.js
@@ -0,0 +1,23 @@
+let loading = document.getElementById("loading");
+let navbar = document.querySelector(".nav");
+
+
+function loadDocument(){
+    
+    loading.style.display = "none";
+}
+
+
+// window.addEventListener("scroll", () => {
+//     if(window.pageYOffset > 100){
+//         navbar.style.width= 100+"%";
+//         navbar.style.height= 70+"px";
+//         navbar.style.background="#222020";
+//     }else
+//     {
+//         navbar.style.width= 100+"%";
+//         navbar.style.height= 70+"px";
+//         navbar.style.background="none";
+//     }
+// });
+
